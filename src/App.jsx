2f11d7720a6d605c0305077066bc3b9194e6bdf3
vinicius <vmm_7@hotmail.com>
2025-05-22 09:58:53 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function App() {
+  return (
+    <main>
+      <div className='pattern'></div>
+      <div className='wrapper'></div>
+      <header>
+        <h1>Find <span className='text-gradient'>Movies</span> You´ll Enjoy</h1>
+      </header>
+
+      <p>Search</p>
+    </main>
+  )
+}
+
+export default App
