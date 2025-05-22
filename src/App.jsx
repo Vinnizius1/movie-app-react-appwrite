451b7d5f792ff406b59e8769e1dd9e824b71af5c
vinicius <vmm_7@hotmail.com>
2025-05-22 10:33:03 -0300

    feat: Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
+import Search from './components/Search'
+import React, { useState } from 'react'
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
     <main>
-      <div className='pattern'></div>
-      <div className='wrapper'></div>
+      <div className='pattern' />
+      <div className='wrapper'>
       <header>
+        <img src="./hero.png" alt="Hero Banner" />
         <h1>Find <span className='text-gradient'>Movies</span> You´ll Enjoy</h1>
       </header>
 
-      <p>Search</p>
+      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
     </main>
   )
 }
