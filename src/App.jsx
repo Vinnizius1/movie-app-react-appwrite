15751c1181268b09d2775f385a4b1f6d0e961973
vinicius <vmm_7@hotmail.com>
2025-05-25 07:43:05 -0300

    feat: implement movie fetching with error handling and loading states

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,83 @@
-import Search from './components/Search'
-import React, { useState } from 'react'
+import Search from "./components/Search";
+import React, { useState, useEffect } from "react";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${API_KEY}`,
+  },
+};
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    // Setup inicial - fora do try
+    /* São operações de setup que devem acontecer independentemente de sucesso ou falha
+      Deixa mais claro o fluxo da função
+      Separa melhor as responsabilidades do código */
+    setIsLoading(true);
+    setMovieList([]);
+    setErrorMessage("");
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch movies");
+      }
+
+      const data = await response.json();
+
+      // "[]" Adicionado para proteção extra contra undefined/null
+      setMovieList(data.results || []);
+
+      // ???
+      /*       if (data.Response === "False") {
+        setErrorMessage(data.Error || "Failed to fetch movies");
+      } */
+    } catch (error) {
+      console.log(`Error feching movies: ${error}`);
+      setErrorMessage("Error fetching movies. Please try again later.");
+    } finally {
+      // O bloco finally é especialmente útil para garantir que setIsLoading(false) seja executado independentemente do resultado da operação.
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
 
   return (
     <main>
-      <div className='pattern' />
-      <div className='wrapper'>
-      <header>
-        <img src="./hero.png" alt="Hero Banner" />
-        <h1>Find <span className='text-gradient'>Movies</span> You´ll Enjoy</h1>
-      </header>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You´ll Enjoy
+          </h1>
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        </header>
 
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
